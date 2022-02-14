9fc92d557dad2f2c0f238bfe51404f768d9be9c3
Cezar Neaga <hi@cezarneaga.eu>
2022-02-14 13:32:43 +0100

    fix some ts errors :)

--- components/drawer/drawer-context.tsx
+++ components/drawer/drawer-context.tsx
@@ -2,7 +2,7 @@ import { useState, createContext, Dispatch, SetStateAction } from 'react'
 
 export const DrawerContext = createContext<{ state: boolean; dispatch: Dispatch<SetStateAction<boolean>> } | undefined>(undefined)
 
-export function DrawerProvider({ children }) {
+export function DrawerProvider({ children }: { children: any }) {
   const [state, dispatch] = useState(false)
   return <DrawerContext.Provider value={{ state, dispatch }}>{children}</DrawerContext.Provider>
 }
