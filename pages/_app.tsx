4e69ce3d9f4d0c842407fab34361efb707f07579
Cezar Neaga <hi@cezarneaga.eu>
2022-03-09 22:40:38 +0100

    add gtag to site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,42 @@
 import 'styles/style.scss'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
 import { NhostAuthProvider } from '@nhost/react-auth'
 import { NhostApolloProvider } from '@nhost/react-apollo'
-
+import Script from 'next/script'
 import { nhost } from '../utils/nhost'
+import * as gtag from '../utils/gtag'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <NhostAuthProvider nhost={nhost}>
       <NhostApolloProvider nhost={nhost}>
+        <Script strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
+        <Script
+          id='gtag-init'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <Component {...pageProps} />
       </NhostApolloProvider>
     </NhostAuthProvider>
